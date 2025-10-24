cdc502ec8519d65248cad0e51dfd24b05dc57a57
JustynaRacinowska <jracinowska@st.swps.edu.pl>
2025-10-24 13:32:14 +0200

    Lab_04 Do zadania 1

--- projekt1/projekt1/projekt1/settings.py
+++ projekt1/projekt1/projekt1/settings.py
@@ -31,12 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'biblioteka.apps.BibliotekaConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
 ]
 
 MIDDLEWARE = [
